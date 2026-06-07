54ab1b811912c87c04cb0aa7780fcf2e9df41ea4
Code-BardProject <tarukian71@gmail.com>
2026-06-07 16:48:50 +0400

    Clean: Remove TestProject test files

--- TestProject/web/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<h1>TestProject Web</h1>',
-  styles: ['h1 { font-family: Arial, sans-serif; }']
-})
-export class AppComponent {}
